c00efebc5f311c02db18fbf1cc7ecfbaec77f032
Mahdi Sharifi <devraymondsh@gmail.com>
2025-10-13 04:15:21 +0330

    Update the nginx config and the payment.sepehr-ai.com.conf nginx config

--- hooks/useGenJob.ts
+++ hooks/useGenJob.ts
@@ -1,12 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 "use client";
 
 import type { JobStatus } from "@/types/jobs";
 import { useRef, useState } from "react";
 import { toast } from "react-toastify";
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
